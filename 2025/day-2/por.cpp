6f8a6e07b3880ef831f39f1bd8cc2f82d4c74d8e
Kamix-08 <pawlowski.kaka@gmail.com>
2025-04-23 18:42:27 +0200

    solve por

--- 2025/day-2/por.cpp
+++ 2025/day-2/por.cpp
@@ -1,10 +1,64 @@
-// 25
-
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
+
+int* arr;
+
+unsigned long long merge(int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int* l_tmp = new int[n1];
+    int* r_tmp = new int[n2];
+
+    for(int i=0; i<n1; i++) l_tmp[i] = arr[l + i];
+    for(int i=0; i<n2; i++) r_tmp[i] = arr[m + 1 + i];
+
+    int i, j;
+    i = j = 0;
+
+    int k = l;
+    unsigned long long s = 0;
+
+    while(i < n1 && j < n2) {
+        if(l_tmp[i] < r_tmp[j]) {
+            arr[k] = l_tmp[i];
+            ++i;
+        } else {
+            arr[k] = r_tmp[j];
+            ++j;
+            s += n1 - i;
+        }
+        ++k;
+    }
+
+    while(i < n1) {
+        arr[k] = l_tmp[i];
+        ++i; ++k;
+    }
+
+    while(j < n2) {
+        arr[k] = r_tmp[j];
+        ++j; ++k;
+    }
+
+    delete[] l_tmp;
+    delete[] r_tmp;
+
+    return s;
+}
+
+unsigned long long merge_sort(int l, int r) {
+    if(l >= r) return 0;
+
+    int m = (l+r)/2;
+    unsigned long long s = 0;
+
+    s += merge_sort(l, m);
+    s += merge_sort(m+1, r);
+
+    return s + merge(l, m, r);
+}
 
 int main()
 {
@@ -14,25 +68,14 @@ int main()
     
     int n;
     cin >> n;
-    
-    vector<int> a, b;
-    a.resize(n);
-    b.resize(n);
 
-    for(int i=0; i<n; i++) {
-        int tmp;
-        cin >> tmp;
-
-        a[i] = b[i] = tmp;
-    }
-
-    sort(b.begin(), b.end());
-
-    int count = 0;
+    arr = new int[n];
     for(int i=0; i<n; i++)
-        count += (a[i] != b[i]);
+        cin >> arr[i];
 
-    cout << count*(count-1)/2;
-    
+    cout << merge_sort(0, n-1);
+
+    delete[] arr;
+
     return 0;
 }
